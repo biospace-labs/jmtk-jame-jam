aa899077f322ad31ef34d905cbad570c2f69fd13
eveehguo <eveehguo@gmail.com>
2023-07-09 12:22:57 -0400

    Slight camera tweaks for better fullscreen feel

--- Assets/FollowPlayerCamera.cs
+++ Assets/FollowPlayerCamera.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class FollowPlayerCamera : MonoBehaviour
 {
     public float _smoothTime = 0.2f;
+    public float verticalOffset = 2f;
     private GameObject _player;
     private Vector3 _velocity = Vector3.zero;
     // Start is called before the first frame update
@@ -22,7 +23,7 @@ public class FollowPlayerCamera : MonoBehaviour
                  gameObject.transform.position, 
                  new Vector3(
                     _player.transform.position.x,
-                    _player.transform.position.y,
+                    _player.transform.position.y + verticalOffset,
                     gameObject.transform.position.z
                     ),
                  ref _velocity, 
