f05c7504875eb5217c5a827243ede3edf4f1ac76
Spacepuffin <Spacepuffin7@gmail.com>
2023-07-09 10:41:19 -0400

    Saw

---
+++ Assets/saw.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class saw : MonoBehaviour
+{
+    public float sawTraverseTime = 1.0f;
+    public float sawDelayTime = 0.3f;
+    public float trackLength = 3f;
+
+    private SpriteRenderer _trackRenderer;
+    private SpriteRenderer _sawRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartMoving();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartMoving()
+    {
+        _trackRenderer = transform.GetChild(0).transform.GetChild(1).GetComponent<SpriteRenderer>();
+        _sawRenderer = transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>();
+        _trackRenderer.size = new Vector2(1.0f / 3, trackLength);
+        _trackRenderer.gameObject.transform.position = _sawRenderer.transform.position + transform.up * trackLength / 2.0f;
+        StartCoroutine(Traverse(transform.position + transform.up,
+            transform.position + transform.up * (trackLength + 1)));
+    }
+
+    IEnumerator Traverse(Vector2 start, Vector2 end)
+    {
+        Debug.Log("traverse " + start + " " + end);  
+        float timeElapsed = 0f;
+        while (timeElapsed < sawTraverseTime)
+        {
+            timeElapsed += Time.deltaTime;
+            _sawRenderer.gameObject.transform.position = Vector2.Lerp(start, end, timeElapsed / sawTraverseTime);
+            yield return null;
+        }
+        yield return new WaitForSeconds(sawDelayTime);
+        StartCoroutine(Traverse(end, start));
+    }
+}
