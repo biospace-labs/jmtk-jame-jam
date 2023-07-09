b599d82e9a6afa7544757d05c64efd3e49364c81
eveehguo <eveehguo@gmail.com>
2023-07-09 12:12:41 -0400

    fix compiler errors + build settings

--- Assets/Character/boi.cs
+++ Assets/Character/boi.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using UnityEditor.Compilation;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
 
