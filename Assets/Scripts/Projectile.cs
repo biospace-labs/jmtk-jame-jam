3138d8bfbb23f10a27c8e54c4c5c10096dbbaa9f
Spacepuffin <Spacepuffin7@gmail.com>
2023-07-09 04:00:33 -0400

    Exploding projectiles
    better collision for projectiles
    
    oops

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,7 +7,9 @@ public class Projectile : MonoBehaviour
     public float lifespan = 5;
     public float force = 10;
     public float time = 0;
+    public float blastRadius = 1;
     private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {   
@@ -15,6 +17,7 @@ public class Projectile : MonoBehaviour
         Vector2 forceDirection = new Vector2(Mathf.Cos(gameObject.transform.eulerAngles.z * Mathf.Deg2Rad),
             Mathf.Sin(gameObject.transform.eulerAngles.z * Mathf.Deg2Rad));
         _rigidbody.AddForce(forceDirection * force, ForceMode2D.Impulse);
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,5 +27,27 @@ public class Projectile : MonoBehaviour
         if (time >= lifespan) {
             Destroy(gameObject.transform.parent.gameObject);
         }
+        _spriteRenderer.transform.up = _rigidbody.velocity.normalized;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") ||
+            other.gameObject.layer == LayerMask.NameToLayer("PlayerHurtbox"))
+        {
+            foreach (ParticleSystem psystem in gameObject.GetComponentsInChildren<ParticleSystem>())
+            {
+                psystem.Play();
+            }
+            gameObject.GetComponent<CircleCollider2D>().radius = blastRadius;
+            StartCoroutine(selfDelete());
+        }
+    }
+
+    private IEnumerator selfDelete() {
+        _spriteRenderer.enabled = false;
+        yield return null;
+        gameObject.GetComponent<CircleCollider2D>().enabled = false;
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 }
