0422d3522be30494624571f5c90ec50f684e4985
eveehguo <eveehguo@gmail.com>
2023-07-11 03:59:19 -0400

    make hazards collision check more efficient

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,27 +7,29 @@ public class Hazard : MonoBehaviour
 {
     public float _knockbackStrength = 25; // KnockbackStrength
     public float _knockdownTime = 1; // Knockdown time in seconds
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnCollisionStay2D(Collision2D other)
     {
-        
+        boi boi = other.gameObject.GetComponentInParent<boi>();
+        if (boi)
+        {
+            BonkThat(boi);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D other)
     {
-        List<Collider2D> colliders = new List<Collider2D>();
-        var filter = new ContactFilter2D();
-        filter.useTriggers = true;
-        filter.SetLayerMask(LayerMask.GetMask("PlayerHurtbox"));
-        gameObject.GetComponent<Rigidbody2D>().OverlapCollider(filter, colliders);
-        foreach (Collider2D trigger in colliders) {
-            boi boi = trigger.gameObject.transform.parent.gameObject.GetComponent<boi>();
-            if (boi != null) {
-                Vector2 knockbackVector = trigger.gameObject.transform.position.x <   gameObject.transform.position.x ? Vector2.left : Vector2.right;
-                boi.GetHit(knockbackVector * _knockbackStrength / 3 + Vector2.up * _knockbackStrength,
-                    _knockdownTime);
-            }
+        boi boi = other.gameObject.GetComponentInParent<boi>();
+        if (boi)
+        {
+            BonkThat(boi);
         }
+    }
+
+    void BonkThat(boi boi)
+    {
+        Vector2 knockbackVector = boi.transform.position.x < transform.position.x ? Vector2.left : Vector2.right;
+        boi.GetHit(knockbackVector * _knockbackStrength / 3 + Vector2.up * _knockbackStrength,
+            _knockdownTime);
     }
 }
